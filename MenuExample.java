36813fb67ed3a6f460fd093fd5c49d2cd9394124
Paul Mugambi <mugambipaul257@gmail.com>
2023-05-17 11:02:32 +0300

    the bugfix for menuexmaple

--- MenuExample.java
+++ MenuExample.java
@@ -6,9 +6,9 @@ public class MenuExample {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
-        int number1;
-        int number2;
-        int logic;
+        int number1 = 0;
+        int number2 = 0;
+        
 
         do {
             System.out.println("Choices Available are:");
@@ -18,21 +18,24 @@ public class MenuExample {
             System.out.println("4 - Multiply");
             System.out.println("0 - Exit");
 
+            System.out.print("Enter Number1: ");
+            number1= scanner.nextInt();
+
+            System.out.print("Enter Number2: ");
+            number2= scanner.nextInt();
+
+
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
 
             if (choice == 1) {
-                System.out.println("Performing addition...");
-                logic = number1 + number2;
+                System.out.println("Performing addition..." + (number1 + number2));
             } else if (choice == 2) {
-                System.out.println("Performing subtraction...");
-                logic = number1 - number2;
+                System.out.println("Performing subtraction..." + (number1 - number2));
             } else if (choice == 3) {
-                System.out.println("Performing division...");
-                logic = number1 / number2;
+                System.out.println("Performing division..." + (number1 / number2));
             } else if (choice == 4) {
-                System.out.println("Performing multiplication...");
-                logic = number1 * number2;
+                System.out.println("Performing multiplication..." + (number1 * number2));
             } else if (choice == 0) {
                 System.out.println("Exiting...");
             } else {
